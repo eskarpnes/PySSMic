10fba0977475976ffcfe0e8b6b475baaf2f15fcb
Erlend Skarpnes <erlendj.skarpnes@gmail.com>
2018-09-17 10:08:51 +0200

    Made manager skeleton

---
+++ manager.py
@@ -0,0 +1,22 @@
+import pykka
+
+
+class Manager:
+    def __init__(self):
+        self.consumers = []
+        self.producers = []
+
+    def register_consumer(self, consumer):
+        pass
+
+    def register_producer(self, producer):
+        pass
+
+    def predict_generated_power(self):
+        pass
+
+    def fetch_producers(self):
+        pass
+
+    def fetch_job(self):
+        pass
