3543d7373e00331bc9ebf572bd4a6d2f90c708fb
Erlend Skarpnes <erlendj.skarpnes@gmail.com>
2018-09-18 06:40:26 +0200

    Added optimizer skeleton code

---
+++ optimizer.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy
+
+
+class Optimizer:
+    def __init__(self):
+        pass
+
+    # The main function that optimizes the schedule. How the schedule and job should be implemented is up for discussion
+    def optimize(self, schedule, job):
+        pass
