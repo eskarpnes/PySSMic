24a384980029c07e7a324220b26f0cb0722269ec
Erlend Skarpnes <erlendj.skarpnes@gmail.com>
2018-09-18 06:40:41 +0200

    Added simulator skeleton code

---
+++ simulator.py
@@ -0,0 +1,15 @@
+import manager
+
+
+class Simulator:
+    def __init__(self):
+        # should be initiated based on some sort of configuration scheme
+        pass
+
+    # Starts a new consumer event
+    def new_event(self):
+        pass
+
+    # Sends out a new weather prediction (every 6 hours)
+    def new_prediction(self):
+        pass
