c3c984a6396e1c48631f90ae98c63005c06d6a2c
eskarpnes <erlendj.skarpnes@gmail.com>
2018-09-19 11:28:42 +0200

    Added core functions

--- consumer.py
+++ consumer.py
@@ -1,21 +1,48 @@
-from pykka import ThreadingActor
+from pykka import ThreadingActor, Timeout
 
 
 class Consumer(ThreadingActor):
-    def __init__(self, producers):
+    def __init__(self, producers, job):
         super(Consumer, self).__init__()
         self.producers = producers
+        self.job = job
 
     # Send a message to another actor in a framework agnostic way
-    def send(self, message):
-        pass
+    def send(self, message, receiver):
+        # Sends a blocking message to producers
+        try:
+            answer = receiver.ask(message, timeout=60)
+            action = answer['action']
+            if action == 'DECLINE':
+                self.request_producer()
+            else:
+                self.register_contract()
+        except Timeout:
+            self.request_producer()
 
     # Receive a message in a framework agnostic way
     def receive(self, message, sender):
-        pass
+        action = message['action']
+        if action == 'BROADCAST':
+            self.producers.append(message['producer'])
+        elif action == 'CANCEL':
+            # TODO Implement renegotiation when a contract is cancelled
+            pass
 
     # Function for selecting a producer for a job
-    def request_producer(self, job):
+    def request_producer(self):
+        # TODO Implement priority queue
+        producer = self.producers.pop()
+        # TODO Check if pykka can send own actor reference as 'sender' field
+        message = {
+            'sender': '',
+            'action': 'REQUEST',
+            'job': self.job
+        }
+        self.send(message, producer)
+
+    def register_contract(self):
+        # TODO Design contract
         pass
 
     # FRAMEWORK SPECIFIC CODE
