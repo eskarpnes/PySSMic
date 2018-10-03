b9d6067d50c60ac7b071598c9885a5332741b407
Magnus Gaustad Eriksson <magnuge@stud.ntnu.no>
2018-10-03 15:55:20 +0200

    changed paths so their not only working in pycharm

--- src/frontend/apps/main.py
+++ src/frontend/apps/main.py
@@ -2,7 +2,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from src.frontend.app import app
+from app import app
 
 # TODO: Add/remove users from the neighbourhood
 # TODO: Number of days simulated
@@ -15,4 +15,4 @@ layout = html.Div(children=[
 ])
 
 
-#@app.callback()
+# @app.callback()
--- src/frontend/index.py
+++ src/frontend/index.py
@@ -2,8 +2,9 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from src.frontend.app import app
-from src.frontend.apps import create_sim, main
+from app import app
+from apps import create_sim
+from apps import main
 
 
 app.layout = html.Div([
@@ -18,9 +19,10 @@ def display_page(pathname):
     if pathname == '/':
         return main.layout
     elif pathname == '/apps/create_sim':
-         return create_sim.layout
+        return create_sim.layout
     else:
         return '404'
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
