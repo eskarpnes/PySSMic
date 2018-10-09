ae022b49c89faf9c1288d2af69b57f992e5c9e77
Sigurd Berglann <sberglann94@gmail.com>
2018-10-09 15:25:24 +0200

    fix simulator

--- src/backend/optimizer.py
+++ src/backend/optimizer.py
@@ -1,6 +1,7 @@
 import logging
 from itertools import chain
 import numpy as np
+import pandas as pd
 
 from scipy import optimize, math
 
@@ -15,21 +16,21 @@ class Optimizer:
     # The main function that optimizes the schedule. How the schedule and job should be implemented is up for discussion
     def optimize(self, schedule):
         objective = np.zeros(len(schedule))
-        return optimize.minimize(self.to_minimize, objective, tol=1.0, method="powell")
+        return optimize.minimize(self.to_minimize, objective, tol=2500.0, method="powell")
 
     def to_minimize(self, schedule):
         indices = list(set(chain.from_iterable(
-            map(lambda x: self.producer.schedule[x].load_profile.index.values.tolist(), range(0, len(schedule))))))
+            map(lambda x: self.producer.schedule[x][1].load_profile.index.values.tolist(), range(0, len(schedule))))))
         indices.sort()
 
         consumed = 0
         produced = 0
         penalty = 0
-        for t in range(indices[-1]):
+        for t in indices:
             consumed_t = 0
             for i, j in enumerate(schedule):
                 if j > 0.0:
-                    consumed_t += load_profile_utils.power(self.producer.schedule[i].load_profile, t)
+                    consumed_t += load_profile_utils.power(self.producer.schedule[i][1].load_profile, t)
             produced_t = load_profile_utils.power(self.producer.prediction, t)
 
             produced += produced_t
--- tests/test_optimizer_pytest.py
+++ tests/test_optimizer_pytest.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
-from src.backend.job import Job
+from src.backend.job import Job, JobStatus
 from src.backend.producer import Producer
 
 
 def test_optimize():
     producer = Producer(1.0)
-    job1 = Job(0, 0, pd.Series(index=[0, 3], data=[40, 40]))
-    job2 = Job(0, 0, pd.Series(index=[2, 4], data=[30, 30]))
-    job3 = Job(0, 0, pd.Series(index=[5, 6], data=[10, 10]))
-    job4 = Job(0, 0, pd.Series(index=[5, 6], data=[40, 40]))
-    job5 = Job(0, 0, pd.Series(index=[7, 10], data=[50, 50]))
-    job6 = Job(0, 0, pd.Series(index=[7, 10], data=[40, 40]))
+    job1 = (None, Job(0, 0, pd.Series(index=[0, 3], data=[40, 40])), JobStatus.active)
+    job2 = (None, Job(0, 0, pd.Series(index=[2, 4], data=[30, 30])), JobStatus.active)
+    job3 = (None, Job(0, 0, pd.Series(index=[5, 6], data=[10, 10])), JobStatus.active)
+    job4 = (None, Job(0, 0, pd.Series(index=[5, 6], data=[40, 40])), JobStatus.active)
+    job5 = (None, Job(0, 0, pd.Series(index=[7, 10], data=[50, 50])), JobStatus.active)
+    job6 = (None, Job(0, 0, pd.Series(index=[7, 10], data=[40, 40])), JobStatus.active)
     producer.schedule = [job1, job2, job3, job4, job5, job6]
     producer.prediction = pd.Series(index=[0, 10], data=[50, 50])
 
