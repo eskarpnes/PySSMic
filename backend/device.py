8e50f10351e66bed6dfe20bf50b8cda1c7b6def4
Magnus Gaustad Eriksson <magnuge@stud.ntnu.no>
2018-10-11 11:21:26 +0200

    added device class

---
+++ backend/device.py
@@ -0,0 +1,7 @@
+
+class Device():
+    def __init__(self, devId, name, template, devType):
+        self.id = devId
+        self.name = name
+        self.template = template
+        self.type = devType
