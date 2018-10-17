48ced0fcd12e8f63bd5f6dfd3887945b00963414
Molly <malfridha@gmail.com>
2018-10-17 13:51:30 +0200

    Page to show output for simulation

---
+++ apps/simulate_esn.py
@@ -0,0 +1,16 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import os
+import flask
+import codecs
+from dash.dependencies import Input, Output
+from app import app
+import dash
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+plotly.tools.set_credentials_file(username='molly95', api_key='trZErTL53vf4hnF1Z2Oi')
+
+
+layout = [
+]
