2afc07625fa7b8e7ba899169d5b606e1bbc5e39a
Molly <malfridha@gmail.com>
2018-10-17 13:50:51 +0200

    Included simulation output page

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ import dash_table_experiments as dt
 from app import app
 from apps import create_sim, create_esn, main, base
 from backend.neighbourhood import Neighbourhood
+from apps import create_sim, create_esn, main, simulate_esn, base
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
@@ -25,6 +26,8 @@ def display_page(pathname):
         return base.header, create_sim.layout, base.footer
     elif pathname == '/apps/create_esn':
         return base.header, create_esn.layout, base.footer
+    elif pathname == '/apps/simulate_esn':
+        return base.header, simulate_esn.layout, base.footer
     else:
         return '404'
 
