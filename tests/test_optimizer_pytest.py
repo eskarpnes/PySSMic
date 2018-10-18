ffee03c28820d106a8f2efc9566d9433e88c67c0
Sigurd Berglann <sberglann94@gmail.com>
2018-10-18 09:32:26 +0200

    add rejection logic

--- tests/test_optimizer_pytest.py
+++ tests/test_optimizer_pytest.py
@@ -1,20 +1,25 @@
 import sys
 import os
 
+from backend.manager import Manager
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 import pandas as pd
 from backend.job import Job, JobStatus
-from backend.manager import Manager
 from backend.producer import Producer
 
 
 def test_optimize():
-    manager = Manager()
-    producer = Producer(1.0, manager)
-    job1 = (None, Job(0, 3, pd.Series(index=[0, 3], data=[50, 50])), JobStatus.active)
-    job2 = (None, Job(0, 3, pd.Series(index=[0, 3], data=[50, 50])), JobStatus.active)
-    producer.schedule = [job1, job2]
-    producer.prediction = pd.Series(index=[0, 6], data=[50, 50])
+    man = Manager()
+    producer = Producer(1.0, man)
+    job0 = (None, Job(0, 0, pd.Series(index=[0, 3], data=[40, 40])), JobStatus.active)
+    job1 = (None, Job(0, 0, pd.Series(index=[2, 4], data=[30, 30])), JobStatus.active)
+    job2 = (None, Job(0, 0, pd.Series(index=[5, 6], data=[10, 10])), JobStatus.active)
+    job3 = (None, Job(0, 0, pd.Series(index=[5, 6], data=[40, 40])), JobStatus.active)
+    job4 = (None, Job(0, 0, pd.Series(index=[7, 10], data=[50, 50])), JobStatus.active)
+    job5 = (None, Job(0, 0, pd.Series(index=[7, 10], data=[40, 40])), JobStatus.active)
+    producer.schedule = [job0, job1, job2, job3, job4, job5]
+    producer.prediction = pd.Series(index=[0, 10], data=[50, 50])
 
     result = producer.optimizer.optimize(producer.schedule)
     assert result.x[0] > 0
