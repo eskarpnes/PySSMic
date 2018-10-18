0a06d188ab6255b0c8f84df3c5c103b94ace3b93
Molly <malfridha@gmail.com>
2018-10-18 11:50:57 +0200

    Added DataTable for contracts

--- apps/simulate_esn.py
+++ apps/simulate_esn.py
@@ -1,12 +1,14 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table_experiments as dt
 import plotly.graph_objs as go
 import pandas as pd
+
 
 from app import app
 
 
-def get_df():
+def get_energy_df():
     return pd.DataFrame({'c1': [384, 827], 'c2': [848, 874]})
 
 
@@ -14,7 +16,6 @@ def energy_use(df):
     df_sum = df.sum(axis=1)
     n = 100/(df_sum[0]+df_sum[1])
     return (
-        html.H1("Energy use"),
         dcc.Graph(
             figure=go.Figure(
                 data=[
@@ -28,10 +29,31 @@ def energy_use(df):
     )
 
 
-layout = (
+def get_contracts():
+    return {"Contract ID": [1], "Contract signed (time)": [12.32], "Start of contract (time)": [14.55], "Consumer": ["c1"], "Producer": ["p1"]}
+
+
+layout = html.Div([
     html.Div(
-        energy_use(get_df())
+        html.H2("Energy use")
+    ),
+    html.Div(
+        energy_use(get_energy_df())
+    ),
+    html.Div(
+        html.H2("Contracts")
+    ),
+    html.Div(
+        dt.DataTable(
+            rows=[get_contracts()],
+            columns=["Contract ID", "Contract signed (time)", "Start of contract (time)", "Consumer", "Producer"],
+            row_selectable=True,
+            filterable=True,
+            sortable=True,
+            selected_row_indices=[],
+            id="datatable"
+        )
     )
-)
+])
 
 
