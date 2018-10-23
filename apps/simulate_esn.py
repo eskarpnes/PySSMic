bd76123227da6f7125281c8d73ef19d506cda4cc
Molly <malfridha@gmail.com>
2018-10-23 10:37:13 +0200

    Added hard coded data simulating a simulation output

--- apps/simulate_esn.py
+++ apps/simulate_esn.py
@@ -83,12 +83,12 @@ def energy_consumption():
                 data=[
                     # TODO: Update to input values from simulator
                     go.Scatter(
-                        x=[1, 2, 3, 4], y=[4, 3, 2, 1],
-                        name="Energy available"
+                        x=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23], y=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+                        name="Energy used"
                     ),
                     go.Scatter(
-                        x=[4, 3, 2, 1], y=[3, 3, 3, 3],
-                        name="Energy used"
+                        x=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23], y=[6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+                        name="Energy available"
                     )
                 ],
                 layout=go.Layout(
