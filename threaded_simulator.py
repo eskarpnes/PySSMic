70ed0f602b1b557866656a670e727a2b9c7581dc
eskarpnes <erlendj.skarpnes@gmail.com>
2018-10-31 14:58:58 +0100

    Windows multiprocessing now works, callbacks are now returns

--- threaded_simulator.py
+++ threaded_simulator.py
@@ -6,37 +6,28 @@ import shutil
 
 
 class ThreadedSimulator:
-
-    def __init__(self, config, callback):
+    def __init__(self, config):
         self.runs = config["runs"]
-        self.callback = callback
         self.config = config
-        self.counter = Value('i', 0)
         print("You have " + str(cpu_count()) + " cores.")
         print("This program will run " + str(self.runs / cpu_count()) + " simulations per core.")
 
     def start(self):
+        processes = []
+        if not os.path.isdir("tmp"):
+            os.makedirs("tmp")
         for i in range(self.runs):
-            sim = Simulator(self.config, self.get_run_results)
-            process = Process(target=sim.start)
+            save_name = os.path.join("tmp", "run" + str(i))
+            process = Process(target=self.start_simulation, args=(save_name,))
             process.start()
+            processes.append(process)
+        for process in processes:
+            process.join()
+        return self.unpickle_array()
 
-    def get_run_results(self, contracts, profiles):
-        with self.counter.get_lock():
-            index = self.counter.value
-            self.counter.value += 1
-            if not os.path.isdir("tmp"):
-                os.makedirs("tmp")
-            pathname = os.path.join("tmp", "run" + str(index))
-            with open(pathname + '.pkl', 'wb') as f:
-                pickle.dump((contracts, profiles), f)
-            self.check_if_done()
-
-    def check_if_done(self):
-        print(str(self.counter.value) + " of " + str(self.runs) + " done.")
-        if self.counter.value == self.runs:
-            contracts, profiles = self.unpickle_array()
-            self.callback(contracts, profiles)
+    def start_simulation(self, name):
+        sim = Simulator(self.config, name)
+        sim.start()
 
     def unpickle_array(self):
         contracts, profiles = [], []
@@ -56,19 +47,13 @@ if __name__ == "__main__":
     start_time = time.time()
     # Hardcoded example
     config = {
-        "neighbourhood": "test",
+        "neighbourhood": "small_test",
         "timefactor": 0.0000000000001,
         "length": 86400,
         "runs": 8
     }
 
-
-    def callback(contracts, profiles):
-        end_time = time.time()
-        print(len(contracts))
-        print(len(profiles))
-        print("Elapsed time: " + str(end_time - start_time) + " seconds.")
-
-
-    sim = ThreadedSimulator(config, callback)
-    sim.start()
+    sim = ThreadedSimulator(config)
+    results = sim.start()
+    print(len(results[0]))
+    print(len(results[1]))
