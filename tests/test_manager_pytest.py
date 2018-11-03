fd32bc28756807b17825809de1e6ca99299cda66
Sigurd Berglann <sberglann94@gmail.com>
2018-11-03 12:27:04 +0100

    add tests

--- tests/test_manager_pytest.py
+++ tests/test_manager_pytest.py
@@ -1,12 +1,15 @@
 import sys
 import os
 
-from simulator import Simulator
+from backend.consumer import Consumer
+from simulator import Simulator, Job
+import pandas as pd
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 from backend.manager import Manager
 from backend.producer import Producer
 from .mocks import MockClock
+
 
 def test_register_producer():
     # Make a manager with no neighbourhood (not needed for testing)
@@ -15,3 +18,26 @@ def test_register_producer():
     producer = Producer("id", manager)
     manager.register_producer("id", producer)
     assert producer in manager.producers
+
+
+def test_terminate_producers():
+    sim = Simulator(dict(), None)
+    manager = Manager(sim)
+    p1 = Producer.start("P1", manager)
+    p2 = Producer.start("P2", manager)
+    manager.register_producer(p1, "P1")
+    manager.register_producer(p2, "P2")
+
+    manager.terminate_producers()
+    assert manager.producers == {}
+
+
+def test_terminate_consumers():
+    sim = Simulator(dict(), None)
+    manager = Manager(sim)
+    job = Job(est=0, lst=0, id="job_id", load_profile=pd.Series())
+    consumer = Consumer.start(producers=[], job=job, manager=manager)
+    manager.consumers = [consumer]
+
+    manager.terminate_consumers()
+    assert manager.consumers == []
