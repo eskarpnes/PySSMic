05a63602819f64f06e4cc32e9fe78f1bb74a0838
Sigurd Berglann <sberglann94@gmail.com>
2018-11-06 10:07:31 +0100

    add integration tests

---
+++ tests/integration/test_simulator_pytest.py
@@ -0,0 +1,22 @@
+import os
+
+from simulator import Simulator
+
+
+def test_simulator():
+    config = {
+        "neighbourhood": "test",
+        "timefactor": 0.000001,
+        "length": 86400,
+        "algo": "50/50"
+    }
+
+    run_name = "test_simulator"
+    s = Simulator(config, run_name)
+    s.start()
+    filename = run_name + ".pkl"
+    file_exists = os.path.isfile(filename)
+    if file_exists:
+        os.remove(filename)
+
+    assert file_exists
