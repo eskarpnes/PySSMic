c94070dde3a6689e42530075aa465f324012f660
eskarpnes <erlendj.skarpnes@gmail.com>
2018-11-07 09:31:55 +0100

    Hide/show extra input, added input validation

--- apps/create_sim.py
+++ apps/create_sim.py
@@ -50,7 +50,7 @@ layout = html.Div(children=[
             ]),
 
             html.Div(className="selectHorizontal", children=[
-                html.Div(className="selectVertical", children=[
+                html.Div(className="selectVertical no-padding", children=[
                     html.Span("Select Optimization Algorithm(s): "),
                     dcc.Dropdown(
                         id="algo",
@@ -62,20 +62,21 @@ layout = html.Div(children=[
                         ],
                     )
                 ]),
-
-                html.Div(className="selectVertical", children=[
-                    html.Div(className="inputText", children=[
-                        html.Span("Step size")
+                html.Div(id="algo-inputs", children=[
+                    html.Div(className="selectVertical", children=[
+                        html.Div(className="inputText", children=[
+                            html.Span("Step size")
+                        ]),
+                        dcc.Input(id="eps", type="float", value=0.0001),
                     ]),
-                    dcc.Input(id="eps", type="float", value=0.0001),
-                ]),
 
-                html.Div(className="selectVertical", children=[
-                    html.Div(className="inputText", children=[
-                        html.Span("Tolerance")
-                    ]),
-                    dcc.Input(id="tol", type="float", value=1000.0),
-                ]),
+                    html.Div(className="selectVertical", children=[
+                        html.Div(className="inputText", children=[
+                            html.Span("Tolerance")
+                        ]),
+                        dcc.Input(id="tol", type="float", value=1000.0),
+                    ])
+                ], style={"display": "none"})
             ]),
 
 
@@ -98,6 +99,15 @@ layout = html.Div(children=[
 
 
 @app.callback(
+    Output("algo-inputs", "style"),
+    [Input("algo", "value")]
+)
+def check_algo_display(algo):
+    if algo == "fifty_fifty":
+        return {"display": "none"}
+    return {"display": "block"}
+
+@app.callback(
     Output(component_id='btn-simulate', component_property='disabled'),
     [Input('neighbourhood', 'value'),
      Input('days', 'value'),
@@ -111,7 +121,14 @@ def check_button_disable(neighbourhood, days, algo, eps, tol, runs):
         days = None
     if runs in ["", "0"]:
         runs = None
-    if None in [neighbourhood, days, algo, eps, tol, runs]:
+    if eps in ["", "0"]:
+        eps = None
+    if tol in ["", "0"]:
+        tol = None
+    if algo in ['L_BFGS_B', 'SLSQP', 'TNC']:
+        if None in [eps, tol]:
+            return True
+    if None in [neighbourhood, days, algo, runs]:
         return True
     else:
         return False
