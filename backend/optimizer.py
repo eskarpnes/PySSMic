6766425363aac292c31c34a8157c5be6a201bd9d
Sigurd Berglann <sberglann94@gmail.com>
2018-11-08 21:35:59 +0100

    optimizer fixes and comments

--- backend/optimizer.py
+++ backend/optimizer.py
@@ -14,6 +14,7 @@ import util.optimizer_utils as utils
 
 
 class Algorithm(Enum):
+    """The currently possible algorithms."""
     fifty_fifty = 'fifty_fifty'
     L_BFGS_B = 'L-BFGS-B'
     SLSQP = 'SLSQP'
@@ -26,12 +27,13 @@ class Optimizer:
         self.options = options
         self.logger = logging.getLogger("src.Optimizer")
         self.differentiated_loads = []
-        self.penalty_factor = 1.0
         self.min_objective_value = float('inf')
+        self.penalty_factor = options["penalty_factor"] if "penalty_factor" in self.options else 0.5
         self.algorithm = Algorithm[options["algo"]]
 
+        # The estimated total produced energy is the last entry of the producer's prediction profile
+        self.objection_value_offset = 0
 
-    # The main function that optimizes the schedule. How the schedule and job should be implemented is up for discussion
     def optimize(self):
         if self.algorithm in [Algorithm.L_BFGS_B, Algorithm.SLSQP, Algorithm.TNC]:
             schedule_times, should_keep = self.basinhopping()
@@ -47,6 +49,7 @@ class Optimizer:
 
     def basinhopping(self):
         self.reset_and_differentiate_loads()
+        self.objection_value_offset = self.producer.prediction.values[-1]
         tol = self.options["tol"] if "tol" in self.options else 1000.0
         eps = self.options["eps"] if "eps" in self.options else 0.001
         now = self.producer.manager.clock.now
@@ -61,51 +64,75 @@ class Optimizer:
 
         should_keep = [True for x in range(len(self.producer.schedule))]
 
-        if objective_value > self.min_objective_value:
-            should_keep[-1] = False
-        else:
+        if np.isinf(self.min_objective_value):
+            # If the solution we have found is bad, and no previous solution has been discovered.
+            if not self.strictly_positive():
+                should_keep[-1] = False
+            else:
+                self.min_objective_value = objective_value
+        elif objective_value < self.min_objective_value:
             self.min_objective_value = objective_value
+        else:
+            should_keep[-1] = False
 
         return schedule_times, should_keep
 
     def fifty_fifty(self):
         should_keep = [True for x in range(len(self.producer.schedule))]
-        need_grid_power = not self.strictly_positive([s['job'].scheduled_time for s in self.producer.schedule])
+        need_grid_power = not self.strictly_positive()
         if random() < 0.5 or need_grid_power:
             should_keep[-1] = False
         schedule_times = [s['job'].scheduled_time for s in self.producer.schedule]
         return schedule_times, should_keep
 
     def objective_function(self, schedule: List[float]):
+        """The function we want to minimize. schedule is a List of start times for the jobs in the schedule."""
         consumed = defaultdict(float)
-
         for i, load in enumerate(self.differentiated_loads):
             for t, p in load.items():
                 if not math.isnan(schedule[i]):
-                    offset = schedule[i]
-                    consumed[int(round(t + offset))] += p
+                    start_time = schedule[i]
+                    # Add power, p(t), to consumed at time 'start_time' plus 't'
+                    consumed[int(round(start_time + t))] += p
 
-        penalty = 0
-        diff = 0
+        # Differentiate and interpolate production profile on the indices found in consumer
         ts = list(consumed.keys())
         produced = utils.differentiate_and_interpolate(self.producer.prediction, ts)
         ts.extend(list(produced.index.values))
-        ts = sorted(ts)
+        ts = sorted(list(set(ts)))
+
+        total_consumed = 0
+        penalty = 0
         for i, t in enumerate(ts):
-            if i == 0:
-                delta = ts[1] - ts[0]
+            if i == len(ts) - 1:
+                delta = 0
             else:
-                delta = t - ts[i - 1]
+                delta = abs(ts[i+1] - ts[i])
+
             p = produced[t]
             c = consumed[t]
-            diff_t = abs(p - c) * delta
-            diff += diff_t
+
+            # Multiply difference between produced and consumed in time 't' by the distance to the previous time index.
+            # This way, differences are weighted based on their time span.
+            total_consumed += c * delta
+
+            # If consumed is greater than produced, we have negative amount of production power.
             if c > p:
+                diff_t = abs(p - c) * delta
                 penalty += diff_t
 
-        return diff + penalty
+        # Return the difference between produced and consumed energy, in addition to the penalty for having negative
+        # power levels. Finally, subtract this from the objection_value_offset, i.e. the sum of the produced power. This
+        # is so that the theoretically minimal value is equal to zero (if we consume all produced energy).
+        score = abs(self.objection_value_offset - total_consumed) + penalty * self.penalty_factor
+        return score
 
-    def strictly_positive(self, schedule):
+    def strictly_positive(self):
+        """Checks if the current schedules yields positive power in every time step (i.e. we always have more produced
+        energy than consumed energy. Mostly the same as the objective function, but terminates faster, and returns a
+        boolean value indicating whether the excess energy function is always positive, given the current schedule."""
+
+        schedule = [s['job'].scheduled_time for s in self.producer.schedule]
         self.reset_and_differentiate_loads()
         consumed = defaultdict(float)
         for i, load in enumerate(self.differentiated_loads):
@@ -122,7 +149,10 @@ class Optimizer:
                 return False
         return True
 
+    #
     def reset_and_differentiate_loads(self):
+        """Re-compute the differentiated and interpolated load profiles"""
+
         indices = list(set(chain.from_iterable(
             map(lambda x: self.producer.schedule[x]['job'].load_profile.index.values.tolist(),
                 range(0, len(self.producer.schedule))))))
