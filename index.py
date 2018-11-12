34bd42e07a46073846eef8442ea6f18df01d9597
Molly <malfridha@gmail.com>
2018-11-12 17:40:38 +0100

    Refactoring index.py

--- index.py
+++ index.py
@@ -4,33 +4,31 @@ from dash.dependencies import Input, Output
 import dash_table_experiments as dt
 
 from app import app
-from backend.neighbourhood import Neighbourhood
 from apps import create_sim, create_esn, main, simulate_esn, base
 
 app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content'),
-    # needed to make it work on create_esn
-    html.Div(dt.DataTable(rows=[{}]), style={'display': 'none'}),
-    html.Div(id="datatableDiv", style={'display': 'none'}),
-    html.Div(id="neighbourhood_div", style={'display': 'none'})
+    dcc.Location(id="url", refresh=False),
+    html.Div(id="page-content"),
+    html.Div(dt.DataTable(rows=[{}]), style={"display": "none"}),
+    html.Div(id="datatableDiv", style={"display": "none"}),
+    html.Div(id="neighbourhood_div", style={"display": "none"})
 ], className="main")
 
 
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
+@app.callback(Output("page-content", "children"),
+              [Input("url", "pathname")])
 def display_page(pathname):
-    if pathname == '/':
+    if pathname == "/":
         return base.header, main.layout, base.footer
-    elif pathname == '/apps/create_sim':
+    elif pathname == "/apps/create_sim":
         return base.header, create_sim.layout, base.footer
-    elif pathname == '/apps/create_esn':
+    elif pathname == "/apps/create_esn":
         return base.header, create_esn.layout, base.footer
-    elif pathname == '/apps/simulate_esn':
+    elif pathname == "/apps/simulate_esn":
         return base.header, simulate_esn.layout, base.footer
     else:
-        return '404'
+        return "404"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
