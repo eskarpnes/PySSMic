9ab31a60f342325dec13fc4bfc58ec6854b8e46d
Molly <malfridha@gmail.com>
2018-11-12 18:05:39 +0100

    Refactoring in apps/base.py

--- apps/base.py
+++ apps/base.py
@@ -1,9 +1,6 @@
-import dash
-from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 
-from app import app
 
 """
 This represents a base file which follows every layout
@@ -12,10 +9,10 @@ This represents a base file which follows every layout
 header = html.Div([
     html.H1(children="CoSSMic Simulator"),
     html.Div([
-        dcc.Link('Home', href='/'),
-        dcc.Link('Create Simulation', href='/apps/create_sim'),
-        dcc.Link("Create neighbourhood", href='/apps/create_esn'),
-        dcc.Link('Results', href='/apps/simulate_esn')
+        dcc.Link("Home", href="/"),
+        dcc.Link("Create Simulation", href="/apps/create_sim"),
+        dcc.Link("Create neighbourhood", href="/apps/create_esn"),
+        dcc.Link("Results", href="/apps/simulate_esn")
     ], className="links")
 ], className="header")
 
