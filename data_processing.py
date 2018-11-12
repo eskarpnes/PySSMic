d46d19de7fa8590eed62cb2be3deb562409573cc
Molly <malfridha@gmail.com>
2018-11-12 14:04:03 +0100

    Refactoring in data processing file

--- data_processing.py
+++ data_processing.py
@@ -10,7 +10,7 @@ import statistics
 def open_file(file_name):
     path = os.path.join(ROOT_DIR, "results", file_name)
     print("opening " + path)
-    with open(path, 'rb') as f:
+    with open(path, "rb") as f:
         res = pickle.load(f)
         contracts = res[0]
         profiles = res[1]
@@ -29,14 +29,14 @@ def get_profiles(file_name):
 
 def rename_columns(contract):
     old_keys = ["id", "time", "time_of_agreement", "load_profile", "job_id", "producer_id"]
-    new_keys = ['Contract ID', 'Contract start time', 'Contract agreement time', 'Energy used (Wh)', 'Consumer ID',
-                'Producer ID']
+    new_keys = ["Contract ID", "Contract start time", "Contract agreement time", "Energy used (Wh)", "Consumer ID",
+                "Producer ID"]
     for i in range(0, len(old_keys)):
         contract[new_keys[i]] = contract.pop(old_keys[i])
     return contract
 
 
-def neighbourhood_to_household(contracts_input, profiles_input, grid_id='grid'):
+def neighbourhood_to_household(contracts_input, grid_id="grid"):
     household_dict_list = []
     for contracts in contracts_input:
         df = pd.DataFrame(contracts)
@@ -44,29 +44,22 @@ def neighbourhood_to_household(contracts_input, profiles_input, grid_id='grid'):
         producers.remove(grid_id)
         consumers = list(df.job_id.unique())
         all_devices = producers + consumers
-
         household_dict = dict()
         for device in all_devices:
-            before = device.index('[') + 1
-            after = device.index(']')
+            before = device.index("[") + 1
+            after = device.index("]")
             household_id = device[before:after]
             if household_id in household_dict:
                 household_dict[household_id].append(device)
             else:
                 household_dict[household_id] = [device]
-
         household_dict_list.append(household_dict)
-
     return household_dict_list
-
-
-# household_dictionary = neigbourhood_to_household(contracts, profiles)
 
 
 def average_list_with_lists(list_of_listwithlists):
     list_of_lists = []
     transposed_list_of_listwithlists = map(list, zip(*list_of_listwithlists))
-    # print(transposed_list_of_listwithlists)
     for tl in transposed_list_of_listwithlists:
         transposed_tl = map(list, zip(*tl))
         temp_list = []
@@ -85,7 +78,6 @@ def average_from_dict(list_of_dicts):
                 new_dict[key].append(value)
             else:
                 new_dict[key] = [value]
-
     final_dict = dict()
     for key in new_dict.keys():
         value_list = new_dict[key]
@@ -94,48 +86,33 @@ def average_from_dict(list_of_dicts):
         for vlt in value_list_transposed:
             new_value_list.append(np.mean(vlt))
         final_dict[key] = new_value_list
-
     return final_dict
 
 
 # Pie chart
-def remote_versus_local(contracts, grid_id='grid'):
-    # Load the contracts into a pandas dataframe
+def remote_versus_local(contracts, grid_id="grid"):
     df = pd.DataFrame.from_dict(contracts)
-
-    # Add a column 'energy_used' for the total energy exchanged during
-    # the contract (last value in load_profile)
-    load_profiles = df['load_profile'].tolist()
+    load_profiles = df["load_profile"].tolist()
     energy_used = []
     for load_profile in load_profiles:
         energy_used.append(load_profile.iloc[len(load_profile.index) - 1])
-    df['energy_used'] = energy_used
-
-    # Get all unique consumer_ids
-    consumer_ids = df['job_id'].tolist()
+    df["energy_used"] = energy_used
+    consumer_ids = df["job_id"].tolist()
     unique_consumer_ids = list(set(consumer_ids))
-
-    # Determine the local and remote energy used
     output_dict = dict()
     for consumer in unique_consumer_ids:
-        # Find all contracts from a specific consumer
-        df_consumer_contracts = df.loc[df['job_id'] == consumer]
-        # Find all local contracts and sum over the energy
-        df_consumer_local = df_consumer_contracts.loc[df_consumer_contracts['producer_id'] != grid_id]
-        local_energy = df_consumer_local['energy_used'].sum()
-        # Find all remote contracts and sum over the energy
-        df_consumer_remote = df_consumer_contracts.loc[df_consumer_contracts['producer_id'] == grid_id]
-        remote_energy = df_consumer_remote['energy_used'].sum()
-        # Write the consumer_id to a dictionary with a list of his local and remote energy
+        df_consumer_contracts = df.loc[df["job_id"] == consumer]
+        df_consumer_local = df_consumer_contracts.loc[df_consumer_contracts["producer_id"] != grid_id]
+        local_energy = df_consumer_local["energy_used"].sum()
+        df_consumer_remote = df_consumer_contracts.loc[df_consumer_contracts["producer_id"] == grid_id]
+        remote_energy = df_consumer_remote["energy_used"].sum()
         output_dict[consumer] = [local_energy, remote_energy]
-
     return output_dict
 
 
 # Divides time index by 60 (NOT NOW!) and gives opportunity to accumulate values
 def change_index_time(energy_list, start_times):
     energy_series = pd.Series([0 for x in range(0, 86460, 60)], [x for x in range(0, 86460, 60)])
-
     for i in range(len(energy_list)):
         energy_values = energy_list[i]
         start_time = start_times[i]
@@ -147,12 +124,9 @@ def change_index_time(energy_list, start_times):
         energy_values = energy_values.interpolate(method="linear")
         energy_values = energy_values.diff().fillna(0)
         energy_series = (energy_series + energy_values).fillna(energy_series).fillna(energy_values)
-
     energy_series = energy_series.rename(lambda x: x // 60)
-
     x_energy = energy_series.index.tolist()
     y_energy = energy_series.values.tolist()
-
     return [x_energy, y_energy]
 
 
@@ -160,52 +134,40 @@ def change_index_time(energy_list, start_times):
 def add_productions(producer_profiles):
     if producer_profiles == []:
         return [], []
-
     produced_series = producer_profiles.pop(-1)
-
     for profile in producer_profiles:
         produced_series += profile
-
     end_time = int(produced_series.index[-1])
     new_indexes = range(0, end_time + 60, 60)
     produced_series = produced_series.reindex(new_indexes)
     produced_series = produced_series.interpolate(method="linear")
     produced_series = produced_series.diff().fillna(0)
-
     produced_series = produced_series.rename(lambda x: x // 60)
-
     x_production = produced_series.index.tolist()
     y_production = produced_series.values.tolist()
-
     return x_production, y_production
 
 
 def energy_over_time(contracts, producer_profiles):
-    # Split data between remote and local
     local_contracts = []
     remote_contracts = []
-
     for contract in contracts:
         if contract["producer_id"] == "grid":
             remote_contracts.append(contract)
         else:
             local_contracts.append(contract)
-
-    # LOCAL CONSUMPTION
+    # Local consumption
     x_consumption_local, y_consumption_local = change_index_time(
         [contract["load_profile"] for contract in local_contracts],
         [contract["time"] for contract in local_contracts]
     )
-
-    # REMOTE CONSUMPTION
+    # Remote consumption
     x_consumption_remote, y_consumption_remote = change_index_time(
         [contract["load_profile"] for contract in remote_contracts],
         [contract["time"] for contract in remote_contracts]
     )
-
-    # PRODUCTION
+    # Production
     x_production, y_production = add_productions(producer_profiles)
-
     # If any of the lists is empty, give at least one point
     if x_consumption_local == [] and y_consumption_local == []:
         x_consumption = [0]
@@ -213,22 +175,14 @@ def energy_over_time(contracts, producer_profiles):
     if x_production == [] and y_production == []:
         x_production = [0]
         y_production = [0]
-
     return [x_consumption_local, y_consumption_local, x_consumption_remote, y_consumption_remote, x_production,
             y_production]
 
 
 def peak_to_average_ratio(contracts, interval=15):
-    # Load the contracts into a dataframe
     df = pd.DataFrame.from_dict(contracts)
-
-    # Change the seconds into minutes (round downwards)
-    agreement_times = df['time'].tolist()
-
-    # Get consumptions per minute
-    x_consumption, y_consumption = change_index_time(df['load_profile'].tolist(), agreement_times)
-
-    # Determine the consumption per quarter (by summing up all consumptions per minute)
+    agreement_times = df["time"].tolist()
+    x_consumption, y_consumption = change_index_time(df["load_profile"].tolist(), agreement_times)
     consumption_per_quarter = []
     quarter_consumption = 0.0
     for i in range(len(x_consumption)):
@@ -236,21 +190,15 @@ def peak_to_average_ratio(contracts, interval=15):
         if x_consumption[i] % interval == 0:
             consumption_per_quarter.append(quarter_consumption)
             quarter_consumption = 0
-
-    # Determine average and peak consumption
     average_consumption = np.mean(consumption_per_quarter)
     max_consumption = max(consumption_per_quarter)
-
     return max_consumption / average_consumption
 
 
 def peak_to_average_ratio_production(profiles, interval=15):
     x_production, y_production = add_productions(profiles)
-
     if [] in [x_production, y_production]:
         return 0
-
-    # Determine the consumption per quarter (by summing up all consumptions per minute)
     consumption_per_quarter = []
     quarter_consumption = 0.0
     for i in range(len(x_production)):
@@ -258,11 +206,8 @@ def peak_to_average_ratio_production(profiles, interval=15):
         if x_production[i] % interval == 0:
             consumption_per_quarter.append(quarter_consumption)
             quarter_consumption = 0
-
-    # Determine average and peak consumption
     average_consumption = np.mean(consumption_per_quarter)
     max_consumption = max(consumption_per_quarter)
-
     return max_consumption / average_consumption
 
 
@@ -301,7 +246,7 @@ def household_execution_local_versus_remote(contracts_input, households, profile
             household_devices = households[i][household]
             contracts_household = []
             for j in range(len(contracts_input[i])):
-                if contracts_input[i][j]['job_id'] in household_devices:
+                if contracts_input[i][j]["job_id"] in household_devices:
                     contracts_household.append(contracts_input[i][j])
             output_household.append(remote_versus_local(contracts_household))
         output.append(output_household)
@@ -319,7 +264,7 @@ def household_execution_energy_over_time(contracts_input, profiles_input, househ
             household_devices = households[i][household]
             contracts_household = []
             for j in range(len(contracts_input[i])):
-                if contracts_input[i][j]['job_id'] in household_devices or contracts_input[i][j]['producer_id'] \
+                if contracts_input[i][j]["job_id"] in household_devices or contracts_input[i][j]["producer_id"] \
                         in household_devices:
                     contracts_household.append(contracts_input[i][j])
             profile_keys = profiles_input[i].keys()
@@ -350,18 +295,18 @@ def get_profiles_for_house(house, profiles):
     return output
 
 
-
 def get_energy_use(run, simulation):
     contracts = open_file(simulation)[0]
     run_contracts = contracts[run]
     grid = 0
     pv = 0
     for contract in run_contracts:
-        if contract.get("producer_id") == 'grid':
+        if contract.get("producer_id") == "grid":
             grid += contract.get("load_profile").values[-1]
         else:
             pv += contract.get("load_profile").values[-1]
     return [round(pv, 2), round(grid, 2), round(pv+grid, 2)]
+
 
 def get_standard_deviation(simulation):
     runs = get_contracts(simulation)
@@ -374,8 +319,7 @@ def get_standard_deviation(simulation):
         total_local_consumption.append(local_consumption)
     return round(statistics.stdev(total_local_consumption), 2)
 
-# timestamp: int
-# return: Day X HH:MM:SS
+
 def convert(timestamp):
     date = datetime.fromtimestamp(timestamp)
-    return 'Day {} {}'.format(date.day, date.strftime('%H:%M:%S'))
+    return "Day {} {}".format(date.day, date.strftime("%H:%M:%S"))
